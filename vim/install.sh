0b4aeadd3cd67df58c01cd412abcea574615f856
Stéphan DONIN <stephan.donin@gmail.com>
2018-04-28 11:14:00 +0900

    Go support
    In Vim:
    - add the plugin vim-go, recommended by Golang
    - install the language server for Go and enable it in the client
    
    In zsh:
    - set the GOPATH
    - add the "bin" directory in $GOPATH to $PATH

--- vim/install.sh
+++ vim/install.sh
@@ -39,6 +39,7 @@ vim +PluginInstall +qall
 
 # Install the dependencies of some plugins
 sudo npm install -g flow-language-server ocaml-language-server prettier
+go get -u github.com/sourcegraph/go-langserver github.com/zmb3/gogetdoc
 reason_cli_package_name='reason-cli@3.1.0-linux'
 if [[ ${platform} = 'mac' ]]
 then
@@ -50,6 +51,7 @@ sudo npm install -g --unsafe-perm ${reason_cli_package_name}
 
 # The plugin LanguageClient-neovim requires some post installation operations
 cd ~/.vim/bundle/LanguageClient-neovim/ && git checkout next && bash ./install.sh
+vim +GoUpdateBinaries +qall
 
 # Mac OS specific
 if [[ ${platform} = 'mac' ]]
