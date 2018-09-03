15ea059d2f4c52d5047069a8bf25347bb0995f4f
Stéphan DONIN <stephan@coursebase.co>
2018-09-03 16:13:53 +0900

    vim: replace reason-language-server with ocaml-language-server

--- vim/install.sh
+++ vim/install.sh
@@ -38,10 +38,8 @@ vim +PlugInstall +qall
 
 # Install the dependencies of some plugins
 sudo npm install -g flow-language-server prettier
+npm install --prefix ${LANG_SERVER_DIR} ocaml-language-server
 go get -u github.com/zmb3/gogetdoc
-mkdir -p ${LANG_SERVER_DIR}/reasonml && \
-  wget https://github.com/jaredly/reason-language-server/releases/download/1.0.2/bin.native.linux -O ${LANG_SERVER_DIR}/reasonml/bin.native.linux && \
-  chmod +x ${LANG_SERVER_DIR}/reasonml/bin.native.linux
 # See https://github.com/reasonml/reasonml.github.io/pull/157 for more details about why the --unsafe-perm tag is
 # required
 sudo npm install -g --unsafe-perm reason-cli@latest-linux bs-platform@latest
--- vim/update.sh
+++ vim/update.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+LANG_SERVER_DIR="~/.language-servers"
+
 vim +PlugUpdate +qall
 
 # Update the dependencies of some plugins
 sudo npm install -g flow-language-server@latest prettier@latest
+npm install --prefix ${LANG_SERVER_DIR} ocaml-language-server@latest
 go get -u github.com/zmb3/gogetdoc
 # See https://github.com/reasonml/reasonml.github.io/pull/157 for more details about why the --unsafe-perm tag is
 # required
