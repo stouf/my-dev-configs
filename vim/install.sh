2b5b4a57b417b3374e8366c1659d38597a370c2d
Stéphan DONIN <stephan@coursebase.co>
2018-11-28 16:23:11 +0900

    vim: use the reason-language-server
    Note that this does not remove the ocaml-language-server, it only
    disables it.

--- vim/install.sh
+++ vim/install.sh
@@ -5,6 +5,21 @@ set -u
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 NPM_TOOLS_DIR="~/.my-npm-tools"
+LANGUAGE_SERVER_DIR="${HOME}/.my-language-servers"
+
+mkdir -p "${LANGUAGE_SERVER_DIR}"
+
+function install-reason-language-server {
+  dldir="/tmp/reason-language-server"
+  dldst="${dldir}/linux.zip"
+  mkdir ${dldir}
+  wget \
+    https://github.com/jaredly/reason-language-server/releases/download/1.4.0/linux.zip \
+    -O ${dldst}
+  unzip ${dldst} -d ${dldir}
+  mv ${dldir}/reason-language-server/reason-language-server.exe ${LANGUAGE_SERVER_DIR}/
+  rm -r ${dldir}
+}
 
 # Already existing configuration file ?
 if [[ -e ~/.vimrc ]]
@@ -25,6 +40,9 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 # Copy the vimrc file
 cp ${DIR}/vimrc ~/.vimrc
 
+# Copy the Language-client configuration file
+cp ${DIR}/language-client-settings.json ${HOME}/.vim/settings.json
+
 # Installing all the plugins
 vim +PlugInstall +qall
 
@@ -32,6 +50,7 @@ vim +PlugInstall +qall
 npm install --prefix ${NPM_TOOLS_DIR} prettier
 npm install --prefix ${NPM_TOOLS_DIR} \
   ocaml-language-server flow-language-server
+install-reason-language-server
 go get -u github.com/zmb3/gogetdoc
 # See https://github.com/reasonml/reasonml.github.io/pull/157 for more details about why the --unsafe-perm tag is
 # required
--- vim/update.sh
+++ vim/update.sh
@@ -4,6 +4,21 @@ set -e
 set -u
 
 NPM_TOOLS_DIR="~/.my-npm-tools"
+LANGUAGE_SERVER_DIR="${HOME}/.my-language-servers"
+
+mkdir -p "${LANGUAGE_SERVER_DIR}"
+
+function install-reason-language-server {
+  dldir="/tmp/reason-language-server"
+  dldst="${dldir}/linux.zip"
+  mkdir ${dldir}
+  wget \
+    https://github.com/jaredly/reason-language-server/releases/download/1.4.0/linux.zip \
+    -O ${dldst}
+  unzip ${dldst} -d ${dldir}
+  mv ${dldir}/reason-language-server/reason-language-server.exe ${LANGUAGE_SERVER_DIR}/
+  rm -r ${dldir}
+}
 
 vim +PlugUpdate +qall
 
@@ -11,6 +26,7 @@ vim +PlugUpdate +qall
 npm install --prefix ${NPM_TOOLS_DIR} prettier@latest
 npm install --prefix ${NPM_TOOLS_DIR} \
   ocaml-language-server@latest flow-language-server@latest
+install-reason-language-server
 go get -u github.com/zmb3/gogetdoc
 # See https://github.com/reasonml/reasonml.github.io/pull/157 for more details about why the --unsafe-perm tag is
 # required
