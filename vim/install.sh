cc3165d80a72f67807da5dca1bf7a86303e44297
Stéphan DONIN <stephan.donin@gmail.com>
2019-01-04 10:58:51 +0900

    vim: use /usr/local/ as user-prefix

--- vim/install.sh
+++ vim/install.sh
@@ -4,10 +4,6 @@ set -e
 set -u
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-NPM_TOOLS_DIR="~/.my-npm-tools"
-LANGUAGE_SERVER_DIR="${HOME}/.my-language-servers"
-
-mkdir -p "${LANGUAGE_SERVER_DIR}"
 
 function install-reason-language-server {
   dldir="/tmp/reason-language-server"
@@ -17,7 +13,7 @@ function install-reason-language-server {
     https://github.com/jaredly/reason-language-server/releases/download/1.4.0/linux.zip \
     -O ${dldst}
   unzip ${dldst} -d ${dldir}
-  mv ${dldir}/reason-language-server/reason-language-server.exe ${LANGUAGE_SERVER_DIR}/
+  sudo mv ${dldir}/reason-language-server/reason-language-server.exe /usr/local/bin/
   rm -r ${dldir}
 }
 
@@ -47,13 +43,12 @@ cp ${DIR}/language-client-settings.json ${HOME}/.vim/settings.json
 vim +PlugInstall +qall
 
 # Install the dependencies of some plugins
-npm install --prefix ${NPM_TOOLS_DIR} prettier
-npm install --prefix ${NPM_TOOLS_DIR} \
-  ocaml-language-server flow-language-server
-install-reason-language-server
-go get -u github.com/zmb3/gogetdoc
 # See https://github.com/reasonml/reasonml.github.io/pull/157 for more details about why the --unsafe-perm tag is
 # required
-sudo npm install -g --unsafe-perm reason-cli@latest-linux bs-platform@latest
+sudo npm install -g --unsafe-perm ocaml-language-server flow-language-server \
+  reason-cli@latest-linux bs-platform@latest
+sudo npm install -g prettier
+install-reason-language-server
+go get -u github.com/zmb3/gogetdoc
 
 echo "Please do not forget to install fzf (https://github.com/junegunn/fzf) if you haven't already done it"
--- vim/update.sh
+++ vim/update.sh
@@ -3,11 +3,6 @@
 set -e
 set -u
 
-NPM_TOOLS_DIR="~/.my-npm-tools"
-LANGUAGE_SERVER_DIR="${HOME}/.my-language-servers"
-
-mkdir -p "${LANGUAGE_SERVER_DIR}"
-
 function install-reason-language-server {
   dldir="/tmp/reason-language-server"
   dldst="${dldir}/linux.zip"
@@ -16,18 +11,17 @@ function install-reason-language-server {
     https://github.com/jaredly/reason-language-server/releases/download/1.4.0/linux.zip \
     -O ${dldst}
   unzip ${dldst} -d ${dldir}
-  mv ${dldir}/reason-language-server/reason-language-server.exe ${LANGUAGE_SERVER_DIR}/
+  sudo mv ${dldir}/reason-language-server/reason-language-server.exe /usr/local/bin/
   rm -r ${dldir}
 }
 
 vim +PlugUpdate +qall
 
 # Update the dependencies of some plugins
-npm install --prefix ${NPM_TOOLS_DIR} prettier@latest
-npm install --prefix ${NPM_TOOLS_DIR} \
-  ocaml-language-server@latest flow-language-server@latest
-install-reason-language-server
-go get -u github.com/zmb3/gogetdoc
 # See https://github.com/reasonml/reasonml.github.io/pull/157 for more details about why the --unsafe-perm tag is
 # required
-sudo npm install -g --unsafe-perm reason-cli@latest-linux bs-platform@latest
+sudo npm install --unsafe-perm -g ocaml-language-server@latest \
+  flow-language-server@latest reason-cli@latest-linux bs-platform@latest
+sudo npm install -g prettier@latest
+install-reason-language-server
+go get -u github.com/zmb3/gogetdoc
